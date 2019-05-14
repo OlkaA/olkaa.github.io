861f4c0dd5334ed8fc8fd359a7d2b8f8fb97e820
olkaa <vfksi_89@mail.ru>
2019-05-14 09:32:02 +0200

    Portfolio

--- js/js.js
+++ js/js.js
@@ -3,7 +3,6 @@ $(document).ready(function () {
     showIFrame('#portfolio');
 });
 function showIFrame(source) {
-    console.log(source);
     $(source).jcarousel({
         scroll: 1,
         initCallback: function (carousel) {
@@ -19,14 +18,11 @@ function showIFrame(source) {
     });
 
     $(source + ' a').click(function () {
-        $('#preview_bg').width($(document).width()).height($(document).height()).show();
-
-        var frameWidth = 700;
-
-        $('#preview_box').html('<iframe id="preview_frame"></iframe>').css('left', ($(document).width() - frameWidth) / 2 + 'px').show();
-        $('#preview_frame').attr('src', $(this).attr('href')).height($(window).height() - 200).width(frameWidth);
+        $('#preview_bg').css('display', 'flex');
+        $('#preview_box').html(`<iframe id="preview_frame" src="${$(this).attr('href')}"></iframe>`).show();
         $('.close-button').show();
     });
+
 
     hideAll('#preview_bg');
     hideAll('.close-button');
@@ -38,89 +34,4 @@ function showIFrame(source) {
             $('.close-button').hide();
         });
     }
-
-    
 }
-
-
-//jQuery(function () {
-//    jQuery('#portfolioJS').jcarousel({
-//        scroll: 1,
-//        initCallback: function (carousel) {
-//            jQuery('#portfolioJS_prev').bind('click', function () {
-//                carousel.prev();
-//                return false;
-//            });
-//            jQuery('#portfolioJS_next').bind('click', function () {
-//                carousel.next();
-//                return false;
-//            });
-//        }
-//    });
-
-//    $('#portfolioJS a').click(function () {
-//        $('#preview_bg').width($(document).width()).height($(document).height()).show();
-
-//        let frameWidth = 700;
-
-//        $('#preview_box').html('<iframe id="preview_frame"></iframe>').css('left', ($(document).width() - frameWidth) / 2 + 'px').show();
-//        $('#preview_frame').attr('src', $(this).attr('href')).height($(window).height() - 200).width(frameWidth);
-//        $('#portfolioJS-button').show();
-//    });
-//    $('#preview_bg').click(function () {
-//        hideAll();
-//    });
-//    $('#portfolioJS-button').click(function () {
-//        hideAll();
-//    });
-//    function hideAll() {
-//        $('#preview_bg').hide();
-//        $('#preview_box').hide();
-//        $('#portfolioJS-button').hide();
-//    }
-//});
-
-//jQuery(function () {
-//    jQuery('#portfolio').jcarousel({
-//        scroll: 1,
-//        initCallback: function (carousel) {
-//            jQuery('#portfolio_prev').bind('click', function () {
-//                carousel.prev();
-//                return false;
-//            });
-//            jQuery('#portfolio_next').bind('click', function () {
-//                carousel.next();
-//                return false;
-//            });
-//        }
-//    });
-
-//    $('#portfolio a').click(function () {
-//        $('#preview_bg').width($(document).width()).height($(document).height()).show();
-
-//        let frameWidth = 700;
-
-//        $('#preview_box').html('<iframe id="preview_frame"></iframe>').css('left', ($(document).width() - frameWidth) / 2 + 'px').show();
-//        $('#preview_frame').attr('src', $(this).attr('href')).height($(window).height() - 200).width(frameWidth);
-//        $('#portfolio-button').show();
-//    });
-//    $('#preview_bg').click(function () {
-//        hideAll();
-//    });
-//    $('#portfolio-button').click(function () {
-//        hideAll();
-//    });
-//    function hideAll() {
-//        $('#preview_bg').hide();
-//        $('#preview_box').hide();
-//        $('#portfolio-button').hide();
-//    }
-//});
-
-
-
-
-//    
-//}
-
-                
