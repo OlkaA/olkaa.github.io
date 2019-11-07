c6ce2ff27c02065bce4d110aa7abc9b2f9cf3137
Olha Afanasieva <afanasievaolja@gmail.com>
2019-11-07 15:25:06 +0100

    github user react

---
+++ github-users/precache-manifest.ba9cb1faf512c75b79306f1440d86e85.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2f2edea7d9fa446769577565da8dae7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab0562e12b5a804cdb4a",
+    "url": "/static/css/main.257b5774.chunk.css"
+  },
+  {
+    "revision": "dbaac43bcfe56f46f4d0",
+    "url": "/static/js/2.9dd04b78.chunk.js"
+  },
+  {
+    "revision": "ab0562e12b5a804cdb4a",
+    "url": "/static/js/main.a1d5636d.chunk.js"
+  },
+  {
+    "revision": "25b8a2888353bd6bc4d5",
+    "url": "/static/js/runtime-main.850247cd.js"
+  }
+]);
--- github-users/src/components/NoResults.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function NoResults() {
-  return <div>No results</div>;
-}
