fd3709612e024568e3c77136e57a6cd8a636f3ef
Olha Afanasieva <afanasievaolja@gmail.com>
2019-11-12 18:00:22 +0100

    famly

---
+++ famly/precache-manifest.d5eb977c182e6d1b5d79e702c88f8b0f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "526070a65dda987950c2602ef9f397c7",
+    "url": "/famly/index.html"
+  },
+  {
+    "revision": "9689dcf605292f4824d1",
+    "url": "/famly/static/css/main.19841a16.chunk.css"
+  },
+  {
+    "revision": "dfafb8f3ee39705e9e84",
+    "url": "/famly/static/js/2.14f4b7ce.chunk.js"
+  },
+  {
+    "revision": "9689dcf605292f4824d1",
+    "url": "/famly/static/js/main.293a3320.chunk.js"
+  },
+  {
+    "revision": "6a278ef10f7ff234206b",
+    "url": "/famly/static/js/runtime-main.da51217d.js"
+  }
+]);
--- famly/service-worker.js
+++ famly/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.17ac09dc736a4976c5d21711e298f0dd.js"
+  "/famly/precache-manifest.d5eb977c182e6d1b5d79e702c88f8b0f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/famly/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
