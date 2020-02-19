563703bc32afe9efa2559b8192a379ae8be8a5a7
Olha Afanasieva <afanasievaolja@gmail.com>
2020-02-19 22:55:23 +0100

    added ocean

---
+++ ocean/precache-manifest.6acb4bd3e9f801c3d94a23eb58679e6a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3595a6fa018e6b97d0d0ce431ef8b690",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b7e9f05b508e0b227277",
+    "url": "./static/css/main.c3ac2812.chunk.css"
+  },
+  {
+    "revision": "c002537b98dcbf110322",
+    "url": "./static/js/2.fed8c795.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "./static/js/2.fed8c795.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7e9f05b508e0b227277",
+    "url": "./static/js/main.67107bd4.chunk.js"
+  },
+  {
+    "revision": "54e36753af035f8931f2",
+    "url": "./static/js/runtime-main.b4fb484b.js"
+  },
+  {
+    "revision": "566d192a164411a9b0c87fd78e7e844b",
+    "url": "./static/media/blue-quotes.566d192a.svg"
+  },
+  {
+    "revision": "86602f000c75e4baebaa3a3536a168b5",
+    "url": "./static/media/cake.86602f00.svg"
+  },
+  {
+    "revision": "e4739484bb7ab87425a5283328311840",
+    "url": "./static/media/pink-quotes.e4739484.svg"
+  },
+  {
+    "revision": "e0d65ea38bd92145d722e2c55329a0ea",
+    "url": "./static/media/smile.e0d65ea3.svg"
+  }
+]);
