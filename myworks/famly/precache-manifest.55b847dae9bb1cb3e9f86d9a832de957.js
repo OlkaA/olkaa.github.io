b95c41eda61ba41165a86366df16d6e02ae4ace2
Olha Afanasieva <afanasievaolja@gmail.com>
2020-03-01 10:46:54 +0100

    added famly test assignment

---
+++ myworks/famly/precache-manifest.55b847dae9bb1cb3e9f86d9a832de957.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa4ec7bed51ed752b7806d0cbcd804eb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a674635e1c682f85508c",
+    "url": "./static/css/main.e54ba48e.chunk.css"
+  },
+  {
+    "revision": "dfafb8f3ee39705e9e84",
+    "url": "./static/js/2.14f4b7ce.chunk.js"
+  },
+  {
+    "revision": "a674635e1c682f85508c",
+    "url": "./static/js/main.dc2afe9e.chunk.js"
+  },
+  {
+    "revision": "67905c21bcd2cf67f1bc",
+    "url": "./static/js/runtime-main.bc904a15.js"
+  }
+]);
