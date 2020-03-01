8d972c1892c4487fecbda33cd2a8fb466bd029de
Olha Afanasieva <afanasievaolja@gmail.com>
2020-03-01 10:22:06 +0100

    added ocean and zendesk test assignments to projects

---
+++ myworks/elevatorZendesk/precache-manifest.976fa08309ff90cc37c4c76f860d8fbe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78ffc4c18f614e374ef84dbc18142c27",
+    "url": "./index.html"
+  },
+  {
+    "revision": "230cbe27aad1ca133200",
+    "url": "./static/css/main.813cf008.chunk.css"
+  },
+  {
+    "revision": "bb9f24989dae6e233843",
+    "url": "./static/js/2.abee8e71.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "./static/js/2.abee8e71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "230cbe27aad1ca133200",
+    "url": "./static/js/main.58014d26.chunk.js"
+  },
+  {
+    "revision": "af19a1d6d22100b06856",
+    "url": "./static/js/runtime-main.9b8a3586.js"
+  }
+]);
