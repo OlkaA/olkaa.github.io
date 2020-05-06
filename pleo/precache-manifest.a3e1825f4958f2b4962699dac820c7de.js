89c29bd467799c215b99e22dc45e2c00077eb086
Olha Afanasieva <afanasievaolja@gmail.com>
2020-05-06 10:31:26 +0200

    added build for pleo for testing purpose

---
+++ pleo/precache-manifest.a3e1825f4958f2b4962699dac820c7de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2052791936e8fa0c207dc7173650b9e0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "baf3ccf42f64d6964224",
+    "url": "./static/css/main.7d4f7b32.chunk.css"
+  },
+  {
+    "revision": "29adee758a77f01becd0",
+    "url": "./static/js/2.d9d5ab20.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.d9d5ab20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "baf3ccf42f64d6964224",
+    "url": "./static/js/main.39734ce0.chunk.js"
+  },
+  {
+    "revision": "1e8c7afc7bf36ad0e5f1",
+    "url": "./static/js/runtime-main.64664b1b.js"
+  }
+]);
