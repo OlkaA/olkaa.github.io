ce495e7672bfb19b4589cea78948a68d64b5f4d8
Olha Afanasieva <afanasievaolja@gmail.com>
2020-07-03 10:51:58 +0200

    pwc challenge

---
+++ pwc/precache-manifest.40e247c3f0839af02beea7784f42329a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95fa4cc4422bcc96ba948bba2c9c3c0c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "91adf346243db53d6f4a",
+    "url": "./static/css/main.1d776c30.chunk.css"
+  },
+  {
+    "revision": "7058a87cb0d1d70cb031",
+    "url": "./static/js/2.76ac1add.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.76ac1add.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91adf346243db53d6f4a",
+    "url": "./static/js/main.b723a8a0.chunk.js"
+  },
+  {
+    "revision": "ce9c5c88c4ebf4fc93c8",
+    "url": "./static/js/runtime-main.b8be5ae8.js"
+  }
+]);
