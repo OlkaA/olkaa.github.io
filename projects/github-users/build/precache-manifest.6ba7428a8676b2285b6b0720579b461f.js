1d8699f3f31e10e9b078c43a01af8a06f2b50953
Olha Afanasieva <afanasievaolja@gmail.com>
2020-07-06 15:04:01 +0200

    build github users

---
+++ projects/github-users/build/precache-manifest.6ba7428a8676b2285b6b0720579b461f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14e94c26403781a66af75e5e8737cb99",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c400bb9ebf6f1fa9abd0",
+    "url": "./static/css/main.070bd4a1.chunk.css"
+  },
+  {
+    "revision": "062df79e8b5e79c45306",
+    "url": "./static/js/2.e85d7e95.chunk.js"
+  },
+  {
+    "revision": "c400bb9ebf6f1fa9abd0",
+    "url": "./static/js/main.59650c6a.chunk.js"
+  },
+  {
+    "revision": "d8de329649912e9c6641",
+    "url": "./static/js/runtime-main.2bc31113.js"
+  }
+]);
