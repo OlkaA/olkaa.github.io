b7bb29e7c7793f6682b7088eab4d1eacc44dbc30
Olha Afanasieva <afanasievaolja@gmail.com>
2020-07-07 20:06:16 +0200

    test

---
+++ projects/githubUsers/build/precache-manifest.1d8d0fe47092370301a6a681491210d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e163380546795fa091d63c4b8302f9ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2bf4244061e5abd75459",
+    "url": "/static/css/main.5da7ec5d.chunk.css"
+  },
+  {
+    "revision": "d376d16d04cb55a68150",
+    "url": "/static/js/2.e8e7f35e.chunk.js"
+  },
+  {
+    "revision": "2bf4244061e5abd75459",
+    "url": "/static/js/main.90347abe.chunk.js"
+  },
+  {
+    "revision": "4a05ec2b28732bbae7fb",
+    "url": "/static/js/runtime-main.35b48817.js"
+  }
+]);
--- projects/githubUsers/src/App.js
+++ projects/githubUsers/src/App.js
@@ -6,7 +6,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <Router basename='/'>
+    <Router basename='/projects/githubUsers/build'>
       <Switch>
         <Route exact path="/">
           <MainComponent />
