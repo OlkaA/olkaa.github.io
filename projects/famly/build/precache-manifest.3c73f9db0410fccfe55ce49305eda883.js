97d348f6eb92611cd722eedeea571cf95bb8b515
Olha Afanasieva <afanasievaolja@gmail.com>
2020-07-08 08:32:16 +0200

    added famly project to portfolio

---
+++ projects/famly/build/precache-manifest.3c73f9db0410fccfe55ce49305eda883.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90a345ffb65bdc83755dff5c1354320c",
+    "url": "/projects/famly/build/index.html"
+  },
+  {
+    "revision": "41e2c9d2db994e91b0ff",
+    "url": "/projects/famly/build/static/css/main.a74640ee.chunk.css"
+  },
+  {
+    "revision": "dfafb8f3ee39705e9e84",
+    "url": "/projects/famly/build/static/js/2.14f4b7ce.chunk.js"
+  },
+  {
+    "revision": "41e2c9d2db994e91b0ff",
+    "url": "/projects/famly/build/static/js/main.0b3123a3.chunk.js"
+  },
+  {
+    "revision": "eec94af9ecf54a107fee",
+    "url": "/projects/famly/build/static/js/runtime-main.ca090747.js"
+  }
+]);
---
+++ projects/famly/src/components/ListOfChildren.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import * as API from '../api/index';
+import { Link } from 'react-router-dom';
+
+class ListOfChildren extends Component {
+    state = {
+        children: [],
+    }
+
+    async componentDidMount() {
+        this.getFetchData();
+    }
+
+    async getFetchData() {
+        const resp = await API.fetchChildrenAPI();
+        const children = resp.children || [];
+        this.setState({ children });
+    }
+
+    render() {
+        return (
+            <ul className="children">
+                {this.state.children.map(child =>
+                    (<li key={child.childId}>
+                        <Link to={(child.checkedIn ? '/out' : '/in') + '/' + child.childId}
+                        >
+                            <div className={child.checkedIn ? 'checked-in-img' : 'not-checked-in'}>
+                                <img src={child.image.small} alt={child.name.fullName} />
+                            </div>
+                            <span>{child.name.fullName}</span>
+                            <p className="pickup-time">{child.pickupTime && child.checkedIn && child.pickupTime.split('T')[1].slice(0, 5)}</p>
+                            <p className={child.checkedIn ? 'checked-in' : 'not-checked-in'}>{child.checkedIn ? 'checked in' : 'not checked in'}</p>
+                        </Link>
+                    </li>))}
+            </ul>
+        );
+    }
+}
+
+export default ListOfChildren;
