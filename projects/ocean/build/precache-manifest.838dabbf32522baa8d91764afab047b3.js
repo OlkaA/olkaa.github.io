add6601d3a15f699afb0d000a3f06900e5467c2c
Olha Afanasieva <afanasievaolja@gmail.com>
2020-09-03 22:35:22 +0200

    added new build for slider

---
+++ projects/ocean/build/precache-manifest.838dabbf32522baa8d91764afab047b3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "464f285c11326e4dbc4d5a5048a7b27b",
+    "url": "/olkaa.github.io/projects/slider/build/index.html"
+  },
+  {
+    "revision": "d608832aff0bca2bd3c5",
+    "url": "/olkaa.github.io/projects/slider/build/static/css/main.c3ac2812.chunk.css"
+  },
+  {
+    "revision": "c002537b98dcbf110322",
+    "url": "/olkaa.github.io/projects/slider/build/static/js/2.fed8c795.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/olkaa.github.io/projects/slider/build/static/js/2.fed8c795.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d608832aff0bca2bd3c5",
+    "url": "/olkaa.github.io/projects/slider/build/static/js/main.4134ee12.chunk.js"
+  },
+  {
+    "revision": "462175e0f0a34d6604b5",
+    "url": "/olkaa.github.io/projects/slider/build/static/js/runtime-main.54d1dbeb.js"
+  },
+  {
+    "revision": "566d192a164411a9b0c87fd78e7e844b",
+    "url": "/olkaa.github.io/projects/slider/build/static/media/blue-quotes.566d192a.svg"
+  },
+  {
+    "revision": "86602f000c75e4baebaa3a3536a168b5",
+    "url": "/olkaa.github.io/projects/slider/build/static/media/cake.86602f00.svg"
+  },
+  {
+    "revision": "e4739484bb7ab87425a5283328311840",
+    "url": "/olkaa.github.io/projects/slider/build/static/media/pink-quotes.e4739484.svg"
+  },
+  {
+    "revision": "e0d65ea38bd92145d722e2c55329a0ea",
+    "url": "/olkaa.github.io/projects/slider/build/static/media/smile.e0d65ea3.svg"
+  }
+]);
