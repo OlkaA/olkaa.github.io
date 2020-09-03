6970968bf9e5e7617a25e81216888d854ff46748
Olha Afanasieva <afanasievaolja@gmail.com>
2020-09-03 22:05:15 +0200

    add eCommerceStore to projects

---
+++ projects/eCommerceStore/scripts/main.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <basket />
+  </div>
+</template>
+
+<script>
+import createBasket from './components/basket.vue';
+import createProductList from './components/products_list.js';
+
+createProductList(createBasket());
+</script>
